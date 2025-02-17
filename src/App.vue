bcbc7e4ae295c695bc91fa14c89b6501dbc30b3f
Raajz49 <raj.koirala@cosys.com.np>
2025-02-17 10:28:47 +0545

    added tailwind

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+  <div class="text-red-600">Tailwind Css</div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<style lang="scss" scoped></style>
